5562402d74f1749697a629d9768ac880adf4662d
Inez Escandon <iescan4@gmail.com>
2020-08-12 23:24:27 -0500

    deploy changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
@@ -13,10 +14,11 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/workouts', {
-	useNewUrlParser: true,
-	useFindAndModify: false,
-});
+let uri = 'mongodb://localhost/workouts';
+if (process.env.NODE_ENV === 'production') {
+	uri = process.env.MONGODB_URI;
+}
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(require('./routes/api-routes.js'));
 app.use(require('./routes/html-routes.js'));
